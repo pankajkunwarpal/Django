c3fcf30769501fa4393061fb026af9b7a83edc5b
Pankaj Kumar <pankajkunwarpalsingh@gmail.com>
2021-10-03 14:32:00 +0530

    Delete HyperJob-Agency directory

--- HyperJob-Agency/hyperjob/resume/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import resume_page, NewResume
-
-
-urlpatterns = [
-    path('resumes', resume_page, name='resumes'),
-    path('resume/new', NewResume.as_view(), name='new_resume')
-]
